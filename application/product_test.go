99b38e0f1b407af57bd3c531f79488b3fd91f82e
Felipe Carvalho <felipee_sc@hotmail.com>
2021-03-30 21:46:51 -0300

    implementing and testing enable

---
+++ application/product_test.go
@@ -0,0 +1,21 @@
+package application_test
+
+import (
+	"github.com/felipeesc/hexagonal-go/application"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Teste"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "o preço deve ser maior que zero para habilitar o produto", err.Error())
+}
