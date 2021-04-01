db37673609ecc304845b767259630d4eda8cab67
Felipe Carvalho <felipee_sc@hotmail.com>
2021-03-31 23:57:30 -0300

    working implementation service

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -11,16 +11,34 @@ import (
 func TestProductService_Get(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-
 	product := mock_application.NewMockProductInterface(ctrl)
 	persistece := mock_application.NewMockProductPersistenceInterface(ctrl)
 	persistece.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
-
-	service := application.ProductService{
-		Persistence: persistece,
-	}
+	service := application.ProductService{Persistence: persistece}
 
 	result, err := service.Get("abc")
 	require.Nil(t, err)
 	require.Equal(t, product, result)
+}
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistece := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistece.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+	service := application.ProductService{Persistence: persistece}
+
+	result, err := service.Create("product 1", 10)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+}
+
+func TestProductService_Enable(t *testing.T) {
+
+}
+
+func TestProductService_Disable(t *testing.T) {
+
 }
